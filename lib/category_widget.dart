ec58ea7217e8fdb6c1fabbf1fa5fe4a62f598a87
Hady Awny <hady.awny@icloud.com>
2023-09-04 12:56:54 +0300

    new project for practice

---
+++ lib/category_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CategoryWidget extends StatelessWidget {
+  const CategoryWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: 100,
+      color: Colors.black,
+    ) ;
+  }
+}
---
+++ lib/home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:practice/category_widget.dart';
+
+class Home extends StatelessWidget {
+
+  List<CategoryWidget> categoryList = [];
+
+  static const String routeName = "home";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+        body: ListView.builder(itemBuilder: buildCategoryWidget,
+          itemCount:categoryList.length,)
+
+    );
+  }
+}
+
+CategoryWidget buildCategoryWidget(BuildContext, int position) {
+  return CategoryWidget();
+}
+
+
